498915ba1d32da749520d8c0065c225ba2cd224f
Underrateddev <rajyudhvir29@gmail.com>
2017-11-16 20:51:27 -0800

    Added check for no internet connection

--- FruitTree/FruitTree/FirstViewController.swift
+++ FruitTree/FruitTree/FirstViewController.swift
@@ -61,6 +61,18 @@ class FirstViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        if Reachability.isConnectedToNetwork(){
+        } else {
+            // Alert the user that there is no internet connection
+            let alert = UIAlertController(title: "No Internet Connection!", message: "App may not function properly", preferredStyle: UIAlertControllerStyle.alert)
+            
+            // Add okay action to alert, to return back to the app
+            alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: nil))
+            
+            // Present the alert to the user
+            self.present(alert, animated: true, completion: nil)
+        }
+        
         // Listen for new fruits in the Firebase database
         // it is aysn.kits will be added one by one;
         //
